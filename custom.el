37e86807ae720c2b10cc5d8ce30246a7443dac9b
Shukry Zablah <shukryzablah@gmail.com>
2020-04-16 23:09:09 -0400

    add interface to package managers

--- custom.el
+++ custom.el
@@ -31,7 +31,7 @@
     (ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe ol-rmail ol-w3m)))
  '(package-selected-packages
    (quote
-    (zenburn-theme poly-org org-clock-csv exwm-config exwm ox-reveal auctex slime org-time-budgets ivy-rich auto-package-update flx counsel ivy vterm yasnippet-snippets circe ghub doom-modeline doom-themes org-mime htmlize epa-file markdown-mode keyfreq which-key dired-rainbow dired-git-info diredfl dired ess-smart-underscore elpy org-gcal use-package restclient ess poly-R paredit queue spinner clojure-mode cider pdf-tools magit)))
+    (helm-system-packages system-packages zenburn-theme poly-org org-clock-csv exwm-config exwm ox-reveal auctex slime org-time-budgets ivy-rich auto-package-update flx counsel ivy vterm yasnippet-snippets circe ghub doom-modeline doom-themes org-mime htmlize epa-file markdown-mode keyfreq which-key dired-rainbow dired-git-info diredfl dired ess-smart-underscore elpy org-gcal use-package restclient ess poly-R paredit queue spinner clojure-mode cider pdf-tools magit)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(safe-local-variable-values (quote ((reftex-default-bibliography "thesis.bib"))))
  '(send-mail-function (quote smtpmail-send-it))
