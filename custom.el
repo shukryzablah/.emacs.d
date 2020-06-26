3c8181090aa3a0de5615c494065594cf46d3c329
Shukry Zablah <shukryzablah@gmail.com>
2020-06-26 00:29:40 -0400

    initial refactor commit; slim down; split init into functions

--- custom.el
+++ custom.el
@@ -3,64 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
- '(custom-safe-themes
-   '("e47c0abe03e0484ddadf2ae57d32b0f29f0b2ddfe7ec810bd6d558765d9a6a6c" "c82d24bfba431e8104219bfd8e90d47f1ad6b80a504a7900cbee002a8f04392f" default))
- '(ess-roxy-template-alist
-   '(("description" . ".. content for \\description{} (no empty lines) ..")
-     ("details" . ".. content for \\details{} ..")
-     ("title" . "")
-     ("param" . "")
-     ("return" . "")))
- '(fci-rule-color "#383838")
- '(geiser-default-implementation 'mit)
- '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
- '(nrepl-message-colors
-   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
- '(objed-cursor-color "#ff6c6b")
- '(org-agenda-files '("~/git/org/outline.org"))
- '(org-checkbox-hierarchical-statistics nil)
- '(org-hierarchical-todo-statistics nil)
- '(org-modules
-   '(org-depend ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe ol-rmail ol-w3m))
- '(org-pretty-entities t)
- '(package-selected-packages
-   '(geiser toml-mode ox-hugo all-the-icons ox-pandoc helm-system-packages system-packages org-ref org-plus-contrib ox-extra zenburn-theme org-clock-csv exwm-config exwm jupyter ox-reveal auctex slime org-time-budgets ivy-rich auto-package-update flx counsel ivy vterm yasnippet-snippets circe ghub doom-modeline doom-themes org-mime htmlize epa-file markdown-mode keyfreq which-key dired-rainbow dired-git-info diredfl dired ess-smart-underscore elpy org-gcal use-package restclient ess poly-R paredit queue spinner clojure-mode cider pdf-tools magit))
- '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
- '(safe-local-variable-values
-   '((Syntax . ANSI-Common-Lisp)
-     (Base . 10)
-     (reftex-default-bibliography "thesis.bib")))
- '(send-mail-function 'smtpmail-send-it)
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   '((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3")))
- '(vc-annotate-very-old-color "#DC8CC3"))
+ '(package-selected-packages (quote (magit vscode-dark-plus-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 151 :width normal)))))
+ )
--- init.el
+++ init.el
@@ -1,3 +1,54 @@
-;; SHUKRY ZABLAH
+;; -*- lexical-binding: t; -*-
 
-(org-babel-load-file "~/.emacs.d/config.org")
+(require 'package)
+
+(defun setup-melpa ()
+  "Add melpa to repositories and call package-initialize afterwards."
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  ; TODO: is the following call necessary? 
+  (package-initialize))
+
+(defun package-install-if-not-already (package)
+  "Install package only if not already installed."
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun setup-custom-file ()
+  "Set file path used for Easy Customization."
+  (setq custom-file "~/.emacs.d/custom.el"))
+
+(defun setup-vscode-theme ()
+  "Install and load vscode-dark-plus-theme."
+  (package-install-if-not-already 'vscode-dark-plus-theme)
+  (load-theme 'vscode-dark-plus t))
+
+(defun setup-zenburn-theme ()
+  "Install and load zenburn-theme"
+  (package-install-if-not-already 'zenburn-theme)
+  (load-theme 'zenburn t))
+
+(defun disable-all-themes ()
+  "Disable all enabled themes."
+  (mapc #'(disable-theme) 'custom-enabled-themes))
+
+(defun change-theme (theme-name)
+  "Disable themes and setup new theme."
+  (disable-all-themes)
+  (setup-theme theme-name))
+
+(defun setup-theme (theme-name)
+  "Setup a specific supported theme."
+  (cond ((equal theme-name "vscode") (setup-vscode-theme))
+	((equal theme-name "zenburn") (setup-zenburn-theme))
+	(t (warn "Theme is not supported: %s" theme-name))))
+
+(defun main ()
+  "Entry point for init file."
+  (setup-melpa) 
+  ;(package-refresh-contents) ; TODO: too slow, but how to deal with outdated cache or when bootstrapping?
+  (mapc #'package-install-if-not-already '(use-package magit))
+  (setup-custom-file) 
+  (setup-theme "vscode")
+  (global-set-key (kbd "C-x g") 'magit-status))
+
+(main)
